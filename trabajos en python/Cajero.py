135ade5e7f2b79f80da8ea89cf7ef56cab8922df
Sebas-r11 <s15601774@gmail.com>
2025-03-02 12:28:13 -0500

    Update Cajero.py

--- trabajos en python/Cajero.py
+++ trabajos en python/Cajero.py
@@ -1,14 +1,3 @@
-#Ejemplo de clases
-#class figura:#se define la clase de figura
-    
-    #def __init__(self,num_lados,longitud):
-        #self.num_lados=num_lados
-        #self.longitud=longitud
-
-    #def hallar_perimetro(self):
-        #return self.num_lados*self.longitud
-#piramide=figura(4,8)
-#res=piramide.hallar_perimetro()
 
 #Ejemplo usuario con clases
 
